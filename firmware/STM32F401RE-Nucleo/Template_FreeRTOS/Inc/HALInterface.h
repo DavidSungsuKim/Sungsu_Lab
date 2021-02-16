03651446970de09957cc5cd08e193c237af803fa
davidsungsukim <sheld2@naver.com>
2021-02-16 15:26:44 -0500

    1. changed the project to a C++ project to support C++ and class interface as well as C. 			firmware/STM32F401RE-Nucleo/Template_FreeRTOS/Src/main.cpp 			firmware/STM32F401RE-Nucleo/Template_FreeRTOS/Inc/HALInterface.h 			firmware/STM32F401RE-Nucleo/Template_FreeRTOS/TrueSTUDIO/STM32F4xx-Nucleo/.cproject 			firmware/STM32F401RE-Nucleo/Template_FreeRTOS/TrueSTUDIO/STM32F4xx-Nucleo/.project 			firmware/STM32F401RE-Nucleo/Template_FreeRTOS/TrueSTUDIO/STM32F4xx-Nucleo/.settings/language.settings.xml 			firmware/STM32F401RE-Nucleo/Template_FreeRTOS/TrueSTUDIO/STM32F4xx-Nucleo/STM32F4xx-Nucleo.elf.launch

--- firmware/STM32F401RE-Nucleo/Template_FreeRTOS/Inc/HALInterface.h
+++ firmware/STM32F401RE-Nucleo/Template_FreeRTOS/Inc/HALInterface.h
@@ -1,9 +1,15 @@
 
-#ifndef _HW_MANAGER_H_
-#define _HW_MANAGER_H_
+#ifndef _HAL_INTERFACE_H_
+#define _HAL_INTERFACE_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 void HALIF_Initialize  (void);
 
+#ifdef __cplusplus
+}
+#endif
 
-
-#endif /* _HW_MANAGER_H_ */
+#endif /* _HAL_INTERFACE_H_ */
