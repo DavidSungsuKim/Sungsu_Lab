7913f7a3fe570b4f35ea692c815b109a03586290
davidsungsukim <sheld2@naver.com>
2021-12-06 21:12:25 -0600

    1. added a few more test scripts.

---
+++ python/test/basic/conditions.py
@@ -0,0 +1,6 @@
+result = 1 in [1,2,3]
+print("Is '1' in the list ?", result)
+
+
+result = 'a' in ['a', 'b', 'c']
+print("Is 'a' in the list ?", result)
---
+++ python/test/basic/while.py
@@ -0,0 +1,17 @@
+num = 5
+while num:
+    print("num=", num)
+    num = num - 1
+
+
+num = 5
+while num:
+    print("num=", num)
+    num = num - 1
+    if num == 3:
+        break
+
+num = 0
+while True:
+    num += 1
+    print("num=", num)
