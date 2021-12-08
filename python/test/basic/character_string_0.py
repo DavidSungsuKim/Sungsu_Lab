9f99ba23ffb2c6adb34a3c1923cbab1bb72cc93c
davidsungsukim <sheld2@naver.com>
2021-12-07 20:41:32 -0600

    1. added more test scripts.

---
+++ python/test/basic/character_string_0.py
@@ -0,0 +1,32 @@
+name = 'Ludwig ' + 'Van ' + 'Beethoven'
+print("My name is ",name)
+
+
+adjective = 'super '
+who = 'a ' + adjective * 3 + 'happy person'
+print("I am ", who)    
+
+
+str = "123456789"
+length = len(str)
+print("length= ", length)
+
+
+str = "123456789"
+index = 0
+print("str[",index,"] =", str[index])
+
+
+str = "123456789"
+print("str[0:2]", str[0:2])
+
+
+age = 10
+str = "I am %d years old " % age
+print(str)
+
+
+height = 210
+weight = 100
+str = "My height is %d cm and my weight is %d kg." %(height, weight)
+print(str)
---
+++ python/test/basic/character_string_1.py
@@ -0,0 +1,13 @@
+sentence = "I have meals {0} times a day." .format(3)
+print(sentence)
+
+sentence = "I have meals {0} times a day. {1} of them are made of rice." .format(3,2)
+print(sentence)
+
+num_meals = 3
+num_rice = 2
+print(f'I have meals {num_meals} times a day. {num_rice} of them are made of rice.')
+
+numbers ='111222333'
+char = '1'
+print("The number of '",char,"' in the list= ", numbers.count(char))
