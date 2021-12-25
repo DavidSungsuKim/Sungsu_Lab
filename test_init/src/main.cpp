440e22f1c6b46ac6db60a2844d015cba88c65ddc
davidsungsukim <sheld2@naver.com>
2021-12-25 16:26:24 -0600

    1. added 'test_init' folder and files to init script test. 	new file:   test_init/.vscode/launch.json 	new file:   test_init/.vscode/tasks.json 	new file:   test_init/src/main.cpp

---
+++ test_init/src/main.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/times.h>
+#include <sys/time.h>
+#include <pthread.h>
+
+void* ThreadProc1(void *arg);
+
+int main (void)
+{
+	int ret = 0;
+	pthread_t	thread1;
+
+	ret = pthread_create( &thread1, NULL, ThreadProc1, NULL );
+	
+	ret = pthread_join( thread1, NULL );
+	printf("task done...ret=%d\r\n", ret);
+
+	return 0;
+}
+	
+void* ThreadProc1(void *arg)
+{
+	int sleep_us = 100000;
+	int i = 0;
+	while(1)
+	{
+		printf("Hello, i=%d\r\n",i++);
+		usleep(sleep_us);
+	}
+
+	return NULL;
+}
