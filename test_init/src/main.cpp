90157eb88e2c8d0b315c580898ee350de4a954e3
davidsungsukim <sheld2@naver.com>
2021-12-25 18:36:30 -0600

    1. changed main.cpp. 	modified:   src/main.cpp

--- test_init/src/main.cpp
+++ test_init/src/main.cpp
@@ -82,7 +82,7 @@ bool ControlLED (bool on)
 	if ( lineGpio < 0 )
 		return false;
 
-	printf("%s, on=%d\r\n", __FUNCTION__, on );
+//	printf("%s, on=%d\r\n", __FUNCTION__, on );
 	gpioWrite( lineGpio, ( on == true) ? 1 : 0 );
 	return success;
 }
