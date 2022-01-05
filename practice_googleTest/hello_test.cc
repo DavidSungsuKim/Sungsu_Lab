6be292e71edf91b21e82773856e5966751c46291
davidsungsukim <sheld2@naver.com>
2022-01-04 20:10:04 -0600

    1. added an example for a test fixture class.

--- practice_googleTest/hello_test.cc
+++ practice_googleTest/hello_test.cc
@@ -1,8 +1,33 @@
 #include <gtest/gtest.h>
 
-TEST(HelloTest, BasicAssertions)
-{
-	EXPECT_STRNE("hello", "world");
+#include <math.h>
 
-	EXPECT_EQ(7*6, 42);
+class MyFixture : public ::testing::Test
+{
+protected:
+	void SetUp() override
+	{
+		i = 1;
+	}
+
+	void TearDown() override
+	{
+		i = 0;
+	}
+
+public:
+	int i;
+};
+
+TEST_F(MyFixture, TestUsingFixture)
+{
+	int j = i;
+	EXPECT_EQ(j, 1);
+}
+
+TEST(HelloTest, TestPow)
+{
+	double i = pow(2, 2);
+
+	EXPECT_EQ(i, 4);
 }
