85c24990c57d79e99b6fa42d520351a8c723fffb
davidsungsukim <sheld2@naver.com>
2022-02-28 19:54:19 -0600

    1. doing practice...

--- practice_C#/PracticeCsharp/PracticeCsharp/Program.cs
+++ practice_C#/PracticeCsharp/PracticeCsharp/Program.cs
@@ -8,9 +8,24 @@ namespace PracticeCsharp
 {
     class Program
     {
+        //Mine instance;
         static void Main(string[] args)
         {
+            Mine instance = new Mine();
+            instance.x = 3;
+
+            int x = 0;
+            int y = 1;
+
             Console.Write("Hello C#\r\n");
+            Console.Write(x + "\r\n");
+            Console.Write(y + "\r\n");
+            Console.Write(instance.x);
         }
+    }
+
+    class Mine
+    {
+        public int x = 0;
     }
 }
