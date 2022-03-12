e9f177ce8398617409d6dc8f3e674477da2b3c93
davidsungsukim <sheld2@naver.com>
2022-03-12 10:29:17 -0600

    1. implemented 'SystemClock_Config()' for STM32F207ZG. This has to be tested later on.

--- firmware/CanOpenSTM32/stm32f2xx_can/Src/main.c
+++ firmware/CanOpenSTM32/stm32f2xx_can/Src/main.c
@@ -219,57 +219,48 @@ main(void) {
 }
 
 /**
- * \brief           System Clock Configuration
- */
+ * \brief System Clock Configuration
+  *         The system Clock is configured as follow :
+  *            System Clock source            = PLL (HSE)
+  *            SYSCLK(Hz)                     = 120000000
+  *            HCLK(Hz)                       = 120000000
+  *            AHB Prescaler                  = 1
+  *            APB1 Prescaler                 = 4
+  *            APB2 Prescaler                 = 2
+  *            HSE Frequency(Hz)              = 8000000
+  *            PLL_M                          = 8
+  *            PLL_N                          = 240
+  *            PLL_P                          = 2
+  *            PLL_Q                          = 5
+  *            VDD(V)                         = 3.3
+  *            Flash Latency(WS)              = 3
+  * @param  None
+  * @retval None
+  */
 void
 SystemClock_Config(void) {
+	RCC_ClkInitTypeDef RCC_ClkInitStruct;
+	RCC_OscInitTypeDef RCC_OscInitStruct;
 
-#if defined (CODES_FOR_STM32F7)
-    LL_FLASH_SetLatency(LL_FLASH_LATENCY_3);
-    while (LL_FLASH_GetLatency() != LL_FLASH_LATENCY_3) {}
+	/* Enable HSE Oscillator and activate PLL with HSE as source */
+	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+	RCC_OscInitStruct.HSEState = RCC_HSE_BYPASS;
+	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+	RCC_OscInitStruct.PLL.PLLM = 8;
+	RCC_OscInitStruct.PLL.PLLN = 240;
+	RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+	RCC_OscInitStruct.PLL.PLLQ = 5;
+	HAL_RCC_OscConfig(&RCC_OscInitStruct);
 
-    /* Enable SMPS supply */
-    LL_PWR_ConfigSupply(LL_PWR_DIRECT_SMPS_SUPPLY);
-    LL_PWR_SetRegulVoltageScaling(LL_PWR_REGU_VOLTAGE_SCALE0);
-
-    /* Enable HSE oscillator */
-    LL_RCC_HSE_Enable();
-    while (!LL_RCC_HSE_IsReady()) {}
-
-    /* Setup PLL */
-    LL_RCC_PLL_SetSource(LL_RCC_PLLSOURCE_HSE);
-    LL_RCC_PLL1P_Enable();
-    LL_RCC_PLL1Q_Enable();
-    LL_RCC_PLL1_SetVCOInputRange(LL_RCC_PLLINPUTRANGE_8_16);
-    LL_RCC_PLL1_SetVCOOutputRange(LL_RCC_PLLVCORANGE_WIDE);
-    LL_RCC_PLL1_SetM(5);
-    LL_RCC_PLL1_SetN(80);
-    LL_RCC_PLL1_SetP(1);
-    LL_RCC_PLL1_SetQ(5);
-    LL_RCC_PLL1_SetR(2);
-    LL_RCC_PLL1_SetFRACN(0);
-    LL_RCC_PLL1FRACN_Enable();
-    LL_RCC_PLL1_Enable();
-    while (!LL_RCC_PLL1_IsReady()) {}
-
-    /* Intermediate AHB prescaler 2 when target frequency clock is higher than 80 MHz */
-    LL_RCC_SetAHBPrescaler(LL_RCC_AHB_DIV_2);
-
-    /* Configure system prescalers */
-    LL_RCC_SetSysClkSource(LL_RCC_SYS_CLKSOURCE_PLL1);
-    LL_RCC_SetSysPrescaler(LL_RCC_SYSCLK_DIV_1);
-    LL_RCC_SetAHBPrescaler(LL_RCC_AHB_DIV_2);
-    LL_RCC_SetAPB1Prescaler(LL_RCC_APB1_DIV_2);
-    LL_RCC_SetAPB2Prescaler(LL_RCC_APB2_DIV_2);
-    LL_RCC_SetAPB3Prescaler(LL_RCC_APB3_DIV_2);
-    LL_RCC_SetAPB4Prescaler(LL_RCC_APB4_DIV_2);
-    LL_SetSystemCoreClock(400000000);
-
-    /* Update the time base */
-    if (HAL_InitTick(TICK_INT_PRIORITY) != HAL_OK) {
-        Error_Handler();
-    }
-#endif
+	/* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2
+	 clocks dividers */
+	RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
+	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+	HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_3);
 }
 
 /**
