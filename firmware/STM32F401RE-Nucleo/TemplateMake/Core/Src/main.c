33e2a3085ee2e1c55a5121b9656cd266a0922623
davidsungsukim <sheld2@naver.com>
2023-05-11 21:22:00 -0600

    1. add json files that are needed for compiling and debugging.

--- firmware/STM32F401RE-Nucleo/TemplateMake/Core/Src/main.c
+++ firmware/STM32F401RE-Nucleo/TemplateMake/Core/Src/main.c
@@ -96,9 +96,8 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-    /* USER CODE END WHILE */
-
-    /* USER CODE BEGIN 3 */
+    HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
+    HAL_Delay(100);	
   }
   /* USER CODE END 3 */
 }
