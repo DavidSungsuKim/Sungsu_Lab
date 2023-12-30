760ca6c30a6e7de1a70811e3e7476614ff79de74
davidsungsukim <sheld2@naver.com>
2023-12-29 20:51:52 -0600

    1. add a basic program that turns LD2 on and off on Nucleo F401RE.   - build : cargo build   - flash : cargo flash --chip stm32f401re
      Next work would be to configure debugging environment.

---
+++ practice_rust/cortex-m4/src/main.rs
@@ -0,0 +1,36 @@
+#![no_main]
+#![no_std]
+extern crate cortex_m;
+extern crate cortex_m_rt as runtime;
+extern crate stm32f4;
+
+use core::panic::PanicInfo;
+use stm32f4::stm32f401::Peripherals;
+
+use cortex_m::asm;
+
+#[no_mangle]
+fn main() -> ! {
+    let per = Peripherals::take().unwrap();
+
+    // Enable the clock for GPIOA
+    per.RCC.ahb1enr.write(|w| w.gpioaen().bit(true));
+    
+    // Configure pin as output
+    per.GPIOA.moder.write(|w| w.moder5().bits(0b01));
+
+    // can't return so we go into an infinite loop here
+    loop {
+        // Toggle the LED output
+        per.GPIOA.odr.modify(|r,w| w.odr5().bit(r.odr5().bit_is_clear()));     
+
+        for _i in 0..10000 {
+             asm::nop()
+        }
+    }
+}
+
+#[panic_handler]
+fn panic(_panic: &PanicInfo<'_>) -> ! {
+    loop {}
+}
