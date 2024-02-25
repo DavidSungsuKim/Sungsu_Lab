13955ee7d6e9044f8f1bc259d5c45ec834a936be
davidsungsukim <sheld2@naver.com>
2024-02-24 20:07:11 -0600

    1. add function doc section.
    2. comment out prints.

--- python/test/basic/change_file_names.py
+++ python/test/basic/change_file_names.py
@@ -1,22 +1,31 @@
+#######################################################################################################
+# brief  This module is in order to convert .mp3 file name for ebs 입트영
+# detail
+#  A .mp3 file name for it is basicallly like this: "입트영 24년 2월 1일.mp3"
+#  And title looks like this: "1 Thu. Car Door Dings / 차량 문콕 테러"
+#  Note that in the titles.txt, anything for 'Weekly Review' must be removed manually beforehand.
+#  And if one of these formats is changed, the code should be changed accordingly to correctly parse
+#  and change file names.
+#######################################################################################################
 
 import os
 
-file_path = 'C:\\Users\\sheld\\Downloads\\입트영2월'
+dir_path = os.getcwd()
 
 # get only files names ending .mp3
-file_names = os.listdir(file_path)
+file_names = os.listdir(dir_path)
 mp3_file_names = [file for file in file_names if file.endswith(".mp3")]
 
 # get the title file
 title_file = "titles.txt"
-title_file_path = file_path + "\\" + title_file
+title_dir_path = dir_path + "\\" + title_file
 
 # get the file names from the title file
 title_name_vec = []
-f_title_file = open(title_file_path, 'r', encoding='utf-8')
+f_title_file = open(title_dir_path, 'r', encoding='utf-8')
 lines = f_title_file.readlines()
 for line in lines:
-    print(line)
+    #print(line)
     
     # create a new file name
     line = line.replace("\n","")
@@ -25,7 +34,7 @@ for line in lines:
     new_title = line[index_slicing:]
     
     # append it to the vector
-    print(new_title)
+    #print(new_title)
     title_name_vec.append(new_title)
 
 for file in mp3_file_names:
@@ -45,27 +54,27 @@ for file in mp3_file_names:
 
     # get initial file names
     file_new = day + '-' + date + '_'
-    print( file_new )
+    #print( file_new )
     
-    file_path_old = file_path + "\\" + file
-    file_path_new = file_path + "\\" + file_new
+    dir_path_old = dir_path + "\\" + file
+    dir_path_new = dir_path + "\\" + file_new
         
     # change the file name with the new one
-    os.rename( file_path_old, file_path_new  )
+    os.rename( dir_path_old, dir_path_new  )
     
 #get new mp3 files again.    
-file_names = os.listdir(file_path)
+file_names = os.listdir(dir_path)
 mp3_file_names = [file for file in file_names if file.endswith("_")] 
 
 i = 0
 for file in mp3_file_names:
     file_name_final = file + title_name_vec[i] + ".mp3"
 
-    file_path_old = file_path + "\\" + file
-    file_path_new = file_path + "\\" + file_name_final
+    dir_path_old = dir_path + "\\" + file
+    dir_path_new = dir_path + "\\" + file_name_final
     
     # change the file name
-    os.rename( file_path_old, file_path_new  )
+    os.rename( dir_path_old, dir_path_new  )
     i += 1
 
 
