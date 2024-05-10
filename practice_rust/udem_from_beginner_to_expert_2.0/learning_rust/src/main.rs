ca67fa663b0d13b91310aee5212cec6e237bc137
SungsuKim <sungsu@attabotics.com>
2024-05-10 15:25:45 -0600

    1. add example of compound data types.

--- practice_rust/udem_from_beginner_to_expert_2.0/learning_rust/src/main.rs
+++ practice_rust/udem_from_beginner_to_expert_2.0/learning_rust/src/main.rs
@@ -16,4 +16,23 @@ fn main() {
     // Type conversion
     let a = 10;
     let b = a as f64;
+
+    /* Compound Data Types */
+    // Strings
+    let fixed_string = "Fixed string";
+    let mut flexible_string = String::from("This string will grow");
+    flexible_string.push('s');
+
+    // Arrays
+    let mut array_1 = [4, 5, 6, 7, 8];
+    let num = array_1[3];
+    println!("{:?}", array_1);
+
+    // Vectors
+    let vec_1: Vec<i32> = vec![4, 5, 6, 8, 9];
+    let num = vec_1[3];
+
+    let my_info = ("Salary", 4000, "Age", 40);
+    let Salary_value = my_info.1;
+    let (salary, salary_value, age, age_value) = my_info;
 }
