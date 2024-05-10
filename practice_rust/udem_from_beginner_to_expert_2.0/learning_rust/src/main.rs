b81e7634f6740544de4b14dcfedcb543f8d09a46
SungsuKim <sungsu@attabotics.com>
2024-05-10 15:51:26 -0600

    1. add examples for Functions and Code Blocks.

--- practice_rust/udem_from_beginner_to_expert_2.0/learning_rust/src/main.rs
+++ practice_rust/udem_from_beginner_to_expert_2.0/learning_rust/src/main.rs
@@ -35,4 +35,33 @@ fn main() {
     let my_info = ("Salary", 4000, "Age", 40);
     let Salary_value = my_info.1;
     let (salary, salary_value, age, age_value) = my_info;
+
+    /* Functions and Code blocks */
+    my_fn("This is my func");
+    let str = "Function call with a variable";
+    my_fn(str);
+    //  my_fn(String::from("str")); // This doesn't compile.
+
+    let answer = multiplication(1, 2);
+    let (mul, add, sub) = basic_math(1, 2);
+
+    let full_name = {
+        let first = "Othani";
+        let last = "Shohei";
+        format!("{first} {last}")
+    };
+
+    println!("{full_name}");
+}
+
+fn my_fn(s: &str) {
+    println!("{s}");
+}
+
+fn multiplication(num1: i32, num2: i32) -> i32 {
+    num1 * num2
+}
+
+fn basic_math(num1: i32, num2: i32) -> (i32, i32, i32) {
+    (num1 * num2, num1 + num2, num1 - num2) // brackets are mandatory!
 }
