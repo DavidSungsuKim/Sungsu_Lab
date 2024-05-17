f8589ed95e9b581f36ac053c0e72ea8c0ce87541
SungsuKim <sungsu@attabotics.com>
2024-05-17 08:53:27 -0600

    1. add examples on borrowing and dereferencing.

--- practice_rust/udem_from_beginner_to_expert_2.0/02_ownership/src/main.rs
+++ practice_rust/udem_from_beginner_to_expert_2.0/02_ownership/src/main.rs
@@ -8,6 +8,15 @@
 ==> more memory safe, no run-time overhead
 */
 
+/*
+- Borrrowing Rules
+    - At any time, you can have either one mutable reference or any number of immutable references.
+    - References must always be valid.
+- Solve out two problems
+    - Data race
+    - Dangling references
+*/
+
 fn main() {
     let vec_1 = vec![1, 2, 3];
 
@@ -23,6 +32,48 @@ fn main() {
     let x = 10;
     stack_function(x);
     println!("x: {x}");
+
+    /* borrowing */
+    let mut vec_1 = vec![4, 5, 6];
+    let ref1 = &vec_1;
+    let ref2 = &vec_1;
+    println!("ref1: {:?}, ref2: {:?}", ref1, ref2);
+    let ref3 = &mut vec_1;
+
+    let mut vec_1 = vec![1, 2, 3];
+    let ref1 = &vec_1;
+    borrows_vec(ref1);
+    let ref2 = &mut vec_1;
+    mutably_borrows_vec(ref2);
+    println!("vec 1 is: {:?}", vec_1);
+
+    /* Dereferencing */
+    let mut some_data = 42;
+    let ref_1 = &mut some_data;
+    let deref_copy = *ref_1;
+    *ref_1 = 13;
+    println!("some_data is: {some_data}, deref_copy is: {deref_copy}");
+
+    let mut heap_data = vec![5, 6, 7];
+    let ref_1 = &heap_data;
+    let ref_2 = ref_1;
+    let ref_3 = ref_1;
+    let deref_copy = ref_1.clone();
+
+    let move_out = ref_1;
+    // let move_out_again = ref_1;
+}
+
+fn borrows_vec(vec: &Vec<i32>) {
+    println!("vec is: {:?}", vec);
+}
+
+fn mutably_borrows_vec(vec: &mut Vec<i32>) {
+    vec.push(10);
+}
+
+fn gives_onwership() -> Vec<i32> {
+    vec![4, 5, 6]
 }
 
 fn take_ownership(vec: Vec<i32>) {
