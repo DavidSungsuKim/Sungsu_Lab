65be2bebbbbf7c040e08d3598f5a9bbc6f724ea2
SungsuKim <sungsu@attabotics.com>
2024-06-14 08:54:28 -0600

    1. add practices.

--- practice_rust/practice_functional_programming/src/main.rs
+++ practice_rust/practice_functional_programming/src/main.rs
@@ -341,3 +341,101 @@ fn main() {
 }
 */
 
+/*
+// Problem 1: Fix the code so that it compiles
+// Solution: 
+
+fn main() {
+    let mut vec_1 = vec![4, 5, 6, 9, 8];
+    for i in vec_1.iter_mut()  { 
+        *i = *i * 2;
+    }
+    println!("{:?}", vec_1);
+}
+*/
+
+/*
+// Problem 1: Convert the code based on the combinators  
+
+fn main() {
+    let numbers = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    let mut result = 0;
+
+    /* The code in the for loop needs to be replaced */ 
+    for &num in &numbers {
+        if num % 2 != 0 {
+            let squared_num = num * num;
+            result += squared_num;
+        }
+    }
+
+    println!("Result without combinators: {}", result);
+
+    let result = numbers
+        .into_iter()
+        .filter(|&num| num % 2 != 0 )
+        .map(|num| num * num)
+        .collect::<Vec<i32>>()
+        .into_iter().sum::<i32>();
+    
+    println!("Result with combinators: {}", result);
+}
+*/
+
+/*
+// -------------------------------------------
+//          - Iterating Through Options
+// -------------------------------------------
+
+fn main() {
+    // ------ Use case 1 -----
+
+    let some_product = Some("laptop");
+    let mut products = vec!["cellphone", "battery", "charger"];
+
+    // Solution 1:
+    // match some_product {
+    //     Some(product) => products.push(product),
+    //     _ => {}
+    // };
+
+    // Solution 2:
+    // if let Some(product) = some_product {
+    //     products.push(product);
+    // }
+
+    // Solution 3:
+    products.extend(some_product);
+    println!("{:?}", products);
+
+    // ------- Use case 2 -----
+    let mut products = vec!["cellphone", "battery", "charger"];
+    let products_iter = products.iter().chain(some_product.iter());
+
+    for prod in products_iter {
+        println!("{:?} ", prod);
+    }
+
+    // ------ Use Case 3 -----
+    let products = vec![Some("charger"), Some("battery"), None, Some("cellphone")];
+
+    // Solution 1;
+    // let mut prod_without_none = Vec::new();
+    // for p in products {
+    //     if p.is_some() {
+    //         prod_without_none.push(p.unwrap());
+    //     }
+    // }
+
+    // Solution 2:
+    // let prod_without_none = products
+    //     .into_iter()
+    //     .filter(|x| x.is_some())
+    //     .map(|x| x.unwrap())
+    //     .collect::<Vec<&str>>();
+
+    // Solution 3:
+    let prod_wihtout_none: Vec<&str> = products.into_iter().flatten().collect();
+    println!("{:?}", prod_wihtout_none);
+}
+*/
