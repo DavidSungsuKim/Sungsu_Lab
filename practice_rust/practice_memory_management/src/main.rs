79c43dc3210fd98c5d48498c13e2c4ae1c7fc16e
SungsuKim <sungsu@attabotics.com>
2024-06-25 08:50:18 -0600

    1. add more practices and etc.

--- practice_rust/practice_memory_management/src/main.rs
+++ practice_rust/practice_memory_management/src/main.rs
@@ -565,3 +565,170 @@ fn main() {
     };
 }
 */
+
+/*
+// -------------------------------------------
+// 		RefCell Smart Pointer
+// -------------------------------------------
+
+use std::{cell::RefCell, rc::Rc};
+
+fn main() {
+    // Example 1: Borrowing rules at run time
+    // let mut x = 50;
+    // let x1 = &x;
+    // let x2 = &x;
+    // let x3 = &mut x;
+
+    // println!("{} {} ", x1, x2);
+
+    let a = RefCell::new(10);
+
+    //{ // add the scope later on
+    let b = a.borrow();
+    let c = a.borrow();
+    //}
+
+    drop(b); // add later on, remove after adding the scope above
+    drop(c); // add later on
+    let d = a.borrow_mut();
+    // drop(d) // add later on
+    //println!("{} {}", b, c); // later on delete this
+    //println!("Value of a is : {:?}", a); // add later on
+
+    // Example 2: Interior mutability
+    // let x = 32;
+    // let x1 = &mut x;
+
+    let a = RefCell::new(10);
+    //let c = *a; // add later on
+    let mut b = a.borrow_mut();
+    *b = 15;
+
+    drop(b); // add later on
+    println!("{:?}", a);
+
+    // Example 3
+    let a = Rc::new(RefCell::new(String::from("c++")));
+    let b = Rc::clone(&a);
+
+    *b.borrow_mut() = String::from("rust");
+    println!("{:?}", a);
+}
+*/
+
+/*
+// Problem 1: fill in the code for TODO's
+
+use std::cell::RefCell;
+fn main() {
+    let data: RefCell<Option<i32>> = RefCell::new(Some(42));
+
+    // TODO: Use borrow_mut to safely modify the value inside the RefCell to None.
+    let mut p = data.borrow_mut();
+    *p = None;
+    drop(p);
+
+    if data.borrow().is_some() {
+        println!("Final value: {:?}", data.borrow());
+    } else {
+        println!("No value present.");
+    }
+}
+*/
+
+/*
+// Problem 2: Fix the lines indicated in the code so that it compiles
+
+use std::cell::RefCell;
+struct Car {
+    model: String,
+    price: u32,
+    status: RefCell<&'static str>,
+}
+
+impl Car {
+    fn new(model: &str, price: u32) -> Self {
+        Car {
+            model: model.to_owned(),
+            price,
+            status: RefCell::new("Available"),
+        }
+    }
+
+    fn sold(&self) {
+        let new_status = match self.price {
+            0..=50000 => "Sold - Economy",
+            50001..=100000 => "Sold - Mid Range",
+            _ => "Sold - Luxury",
+        };
+        *self.status.borrow_mut() = new_status;
+    }
+}
+
+fn main() {
+    let car = Car::new("Sedan", 75000);
+    car.sold();
+    println!("Car Status: {}", car.status.borrow()); // Fix this line
+}
+*/
+
+/*
+// Problem 3: The code below will compile. However it will panic at execution time.
+// Task 1: Your first ttask is to add some code so that the it does not panice at execution time
+// Task 2: The value at the last print line will not be displayed.
+// Instead of value, <Borrowed> will be displayed.
+// Add appropriate code so that the value of x is being displayed.
+
+use std::cell::RefCell;
+
+fn main() {
+    let x = RefCell::new(5);
+    let x_ref1 = x.borrow();
+    let x_ref2 = x.borrow();
+    println!("x_ref1: {}, x_ref2: {}", x_ref1, x_ref2);
+
+    /* Code for Task 1 */
+    drop(x_ref1);
+    drop(x_ref2);
+
+    let mut x_ref3 = x.borrow_mut();
+    *x_ref3 = 6;
+
+    /* Code for Task 2 */
+    drop(x_ref3);
+
+    println!("Stored value: {:?}", x);
+}
+*/
+
+// -------------------------------------------
+// 		RefCell Example
+// -------------------------------------------
+use std::{cell::RefCell, rc::Rc};
+
+#[derive(Debug)]
+struct File {
+    active_user: u32,
+}
+
+#[derive(Debug)]
+struct User {
+    file: Rc<RefCell<File>>,
+}
+
+fn main() {
+    let mut txt_file = Rc::new(RefCell::new((File { active_user: 0 })));
+
+    let mut user_1 = User {
+        file: Rc::clone(&txt_file),
+    };
+    user_1.file.borrow_mut().active_user += 1;
+    println!("Active users: {:?}", txt_file.borrow().active_user);
+
+    let mut user_2 = User {
+        file: Rc::clone(&txt_file),
+    };
+    user_2.file.borrow_mut().active_user += 1;
+    println!("Active users: {:?}", txt_file.borrow().active_user);
+}
