e0cca26caf2f9ba1bf7cdc7f62c435a56b2ce124
davidsungsukim <sheld2@naver.com>
2025-01-31 16:58:40 -0600

    1. try supporting the uart in the async mode.

--- firmware/nucleo_l432kc_rust2/src/main.rs
+++ firmware/nucleo_l432kc_rust2/src/main.rs
@@ -1,12 +1,19 @@
 #![no_std]
 #![no_main]
 
+use core::fmt::Write;
 use defmt::*;
 use embassy_executor::Spawner;
 use embassy_stm32::gpio::{Level, Output, Speed};
-
+use embassy_stm32::usart::{Config, Uart};
+use embassy_stm32::{bind_interrupts, peripherals, usart};
 use embassy_time::Timer;
+use heapless::String;
 use {defmt_rtt as _, panic_probe as _};
+
+bind_interrupts!(struct Irqs {
+    USART2 => usart::InterruptHandler<peripherals::USART2>;
+});
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
@@ -15,11 +22,29 @@ async fn main(_spawner: Spawner) {
 
     let mut led = Output::new(p.PB3, Level::High, Speed::Low);
 
+    let config = Config::default();
+    let mut usart = Uart::new(p.USART2, p.PA3, p.PA2, Irqs, p.DMA1_CH7, p.DMA1_CH6, config).unwrap();
+
+    let mut i = 0;
     loop {
         led.set_high();
         Timer::after_millis(30).await;        
         led.set_low();
         let future = Timer::after_millis(1000);
         future.await;
+
+        println!("{}", i);
+        i += 1;
+
+        // for n in 0u32.. {
+        //     let mut s: String<128> = String::new();
+        //     core::write!(&mut s, "Hello DMA World {}!\r\n", n).unwrap();
+        //     //info!("Writing...");
+        //     usart.write(s.as_bytes()).await.ok();
+        // }
+
+        let mut s: String<128> = String::new();
+        core::write!(&mut s, "Hello DMA World {}!\r\n", i).unwrap();
+        usart.write(s.as_bytes()).await.ok();
     }
 }
