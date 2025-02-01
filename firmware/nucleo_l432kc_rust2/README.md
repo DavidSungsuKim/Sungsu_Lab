2abbd8e814140f0a448a040b7f2c7bbfd8241ba8
davidsungsukim <sheld2@naver.com>
2025-01-31 20:06:36 -0600

    1. cleanup.
    2. add the author part in the Cargo.toml.

---
+++ firmware/nucleo_l432kc_rust2/README.md
@@ -0,0 +1,16 @@
+# Tips
+## How to use macros
+   You can use these macros to see printouts in the command line.
+      info!(""Hello World!={}", 1);
+      debug!("debug info={}", 1);
+      warn!(" ... ")
+      error!(" ... ")
+
+
+# Nucleo Setup
+## Serial interface
+   Module - USART2
+   Pin - PA3 (Rx) and PA2 (Tx)
+
+## LED
+   Pin - PB3
--- firmware/nucleo_l432kc_rust2/src/main.rs
+++ firmware/nucleo_l432kc_rust2/src/main.rs
@@ -2,7 +2,6 @@
 #![no_main]
 
 use core::fmt::Write;
-use defmt::*;
 use embassy_executor::Spawner;
 use embassy_stm32::gpio::{Level, Output, Speed};
 use embassy_stm32::usart::{Config, Uart};
@@ -18,33 +17,37 @@ bind_interrupts!(struct Irqs {
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     let p = embassy_stm32::init(Default::default());
-    info!("Hello World!");
 
     let mut led = Output::new(p.PB3, Level::High, Speed::Low);
 
     let config = Config::default();
-    let mut usart = Uart::new(p.USART2, p.PA3, p.PA2, Irqs, p.DMA1_CH7, p.DMA1_CH6, config).unwrap();
+    let mut serial = Uart::new(p.USART2, p.PA3, p.PA2, Irqs, p.DMA1_CH7, p.DMA1_CH6, config).unwrap();
+    let mut s: String<128> = String::new();
+
+    // Clear the screen
+    core::write!(&mut s, "\x1b[H\x1b[2J").unwrap();
+    serial.write(s.as_bytes()).await.ok();
+    s.clear();
 
     let mut i = 0;
-    loop {
-        led.set_high();
-        Timer::after_millis(30).await;        
-        led.set_low();
-        let future = Timer::after_millis(1000);
-        future.await;
-
-        println!("{}", i);
-        i += 1;
-
-        // for n in 0u32.. {
-        //     let mut s: String<128> = String::new();
-        //     core::write!(&mut s, "Hello DMA World {}!\r\n", n).unwrap();
-        //     //info!("Writing...");
-        //     usart.write(s.as_bytes()).await.ok();
-        // }
-
-        let mut s: String<128> = String::new();
+    loop {        
+        // Write to the serial. Transmission starts immediately.
         core::write!(&mut s, "Hello DMA World {}!\r\n", i).unwrap();
-        usart.write(s.as_bytes()).await.ok();
+        let future = serial.write(s.as_bytes());
+    
+        // Turn on the LED 
+        led.set_high();
+
+        // Wait for the serial to finish writing
+        future.await.ok();
+        s.clear();
+
+        // Wait for 1 seconds
+        Timer::after_millis(1000).await;   
+
+        // Turn off the LED
+        led.set_low();
+
+        i += 1;
     }
 }
