a4fc83cd0577dbc40469b9122e889951b86be97c
davidsungsukim <sheld2@naver.com>
2025-02-12 21:18:02 -0600

    1. implement the cli command parser.

--- firmware/nucleo_l432kc_rust2/README.md
+++ firmware/nucleo_l432kc_rust2/README.md
@@ -10,7 +10,14 @@
 # Nucleo Setup
 ## Serial interface
    Module - USART2
-   Pin - PA3 (Rx) and PA2 (Tx)
+	Pins (NucleoL432KC)
+		### TX : A7 (PA2)
+		### RX : A2 (PA3)
+	Baudrate : 115200
+	Line feed setting in Teraterm
+		### TX : CR+LF
+		### RX : CR
+	When connecting a UART-to-USB module to the laptop, make sure the com port is not of Nucleo's.
 
 ## LED
    Pin - PB3
--- firmware/nucleo_l432kc_rust2/src/main.rs
+++ firmware/nucleo_l432kc_rust2/src/main.rs
@@ -10,10 +10,17 @@ use embassy_stm32::usart::{Config, Uart, UartRx};
 use embassy_stm32::mode::Async;
 use embassy_stm32::{bind_interrupts, peripherals, usart};
 use embassy_time::{Timer, Instant};
-use heapless::String;
+use heapless::{String, Vec};
 use {defmt_rtt as _, panic_probe as _};
 
+// constants
 const SIZE_BUFFER: usize = 128;
+const MAX_CLI_ARGS: usize = 20;
+const SIZE_CLI_RX_BUFFER: usize = 64;
+
+// aliases
+type FixedStringSlices = Vec<String<SIZE_CLI_RX_BUFFER>, MAX_CLI_ARGS>;
+type StringCLI = String<SIZE_CLI_RX_BUFFER>;
 
 bind_interrupts!(struct Irqs {
     USART2 => usart::InterruptHandler<peripherals::USART2>;
@@ -31,14 +38,20 @@ async fn led_task(mut led: Output<'static>) {
 
 #[embassy_executor::task]
 async fn cli_task(mut rx: UartRx<'static, Async>) {
+
+    let mut cli_buffer: StringCLI = String::new();
+    cli_buffer.clear();
+
     loop {
+        // wait a character
         let mut buffer = [0u8; 1];
         let _ = rx.read(&mut buffer).await;
 
-        if buffer[0] == 0x0D {
-            debug!("Enter pressed");
-        } else {
-            debug!("Received: {}", buffer[0] as char);
+        // parse the command
+        if let Some(slices) = parse_cli_command(buffer[0], &mut cli_buffer) {            
+            for slice in slices.iter() {
+                debug!("slice: {:?}", slice);
+            }
         }
     }
 }
@@ -53,7 +66,6 @@ async fn main(spawner: Spawner) {
     let serial = Uart::new(p.USART2, p.PA3, p.PA2, Irqs, p.DMA1_CH7, p.DMA1_CH6, config).unwrap();
 
     let (mut serial_tx, mut serial_rx) = serial.split();
-
     let mut str_buffer: String<SIZE_BUFFER> = String::new();
 
     // Spawn the LED task
@@ -62,6 +74,7 @@ async fn main(spawner: Spawner) {
 
     let mut i = 0;
     loop {    
+        /*
         let delay_fut = Timer::after_millis(100);
 
         core::write!(&mut str_buffer, "Serial DMA{}!\r\n", i).unwrap();
@@ -74,6 +87,7 @@ async fn main(spawner: Spawner) {
         str_buffer.clear();
       
         let elapsed = start.elapsed();
+        */
         //debug!("Elapsed: {:?}", elapsed.as_millis());
 
         //debug!("time2");
@@ -81,5 +95,40 @@ async fn main(spawner: Spawner) {
         //debug!("time2 wait done");
 
         i += 1;
+    }
+}
+
+fn parse_cli_command(ch: u8, buffer: &mut StringCLI) -> Option<FixedStringSlices> {
+    if ch != b'\r' {
+        // push byte into the string buffer
+        buffer.push(ch as char).unwrap();
+        None
+    } else {
+        buffer.push(' ').unwrap(); // to fix a problem with the last number to be treated as a string when parsed.
+    
+        let mut slices: FixedStringSlices = Vec::new();
+        let mut start = 0;
+
+        for (index, char) in buffer.char_indices() {
+            if char == ' ' {
+                if start != index {
+                    let slice_str = &buffer[start..index];
+                    let mut slice_string: StringCLI = String::new();
+                    slice_string.push_str(slice_str).unwrap();
+                    let _ = slices.push(slice_string);
+                }
+                start = index + 1;
+            }
+        }
+
+        if start < buffer.len() {
+            let slice_str = &buffer[start..];
+            let mut slice_string: StringCLI = String::new();
+            slice_string.push_str(slice_str).unwrap();
+            let _ = slices.push(slice_string);
+        }
+
+        buffer.clear();
+        Some(slices)
     }
 }
