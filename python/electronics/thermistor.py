f6451d097d80e08c9c5be18d6d33c0be41c89fe3
SungsuKim <sungsu@attabotics.com>
2025-04-04 15:19:17 -0600

    1. improve the interface a little bit.

--- python/electronics/thermistor.py
+++ python/electronics/thermistor.py
@@ -4,18 +4,29 @@ import math
 # 0.218 --> 254.9
 # 4.159 --> 0.156
 
-V_measured = 3.86
-Rt = -7480 * V_measured / ( 13 * V_measured - 55 )
-Beta = 3950
-R0 = 10000
-T0 = 298.15 # Kelvin (25 degrees Celsius)
+def calculate_temperature(V_measured, printLog = True):
+    # Constants
+    R0 = 10000   # Resistance at T0 (25 degrees Celsius)
+    Beta = 3950  # Beta value for the thermistor
+    T0 = 298.15  # Reference temperature in Kelvin (25 degrees Celsius)
 
-print("Rt={}, R0={}".format(Rt, R0))
+    # Calculate Rt using the measured voltage
+    Rt = -7480 * V_measured / (13 * V_measured - 55)
 
-num = 1
-den = 1 / T0 + 1 / Beta * math.log( Rt / R0 )
-T = num / den
+    # Calculate temperature in Kelvin
+    num = 1
+    den = 1 / T0 + 1 / Beta * math.log(Rt / R0)
+    T = num / den
 
-print( "Temperature in Kelvin: ", T )
-print( "Temperature in Celsius: ", T - 273.15 )
+    temp = T - 273.15
+    if printLog == True:
+      print(f"V={V_measured:.3f}[V], Temperature = {temp:.3f}[C]")
+    
+    return T - 273.15  # Return temperature in Kelvin and Celsius
+
+if __name__ == "__main__":
+   # Example usage
+   calculate_temperature(3.88)
+   calculate_temperature(3.84)
+
 
